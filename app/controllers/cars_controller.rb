324289a8dfb8c8ee886c25e4109572cbbf8683b4
andyjzhong <andy.zhong@libertymutual.com>
2017-04-28 14:50:11 -0400

    Fixed Create Cars Functionality
    Able to create cars now because before I was not passing in a user ID.
    After adding in the current_user.cars.build I was able to create
    successfully.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -15,7 +15,13 @@ class CarsController < ProtectedController
 
   # POST /cars
   def create
-    @car = Car.new(car_params)
+    # @car = Car.new(car_params)
+    # Array of current user's car objects
+
+    # Build method allows you to create a new car and also build an association between user and new car.
+      # @car = Car.new(car_params)
+      # @car.user_id = current_user.id
+    @car = current_user.cars.build(car_params)
 
     if @car.save
       render json: @car, status: :created, location: @car
@@ -41,7 +47,8 @@ class CarsController < ProtectedController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_car
-      @car = Car.find(params[:id])
+      # @car = Car.find(params[:id])
+      @car = current_user.cars.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
--- scripts/cars-scripts/create_cars.sh
+++ scripts/cars-scripts/create_cars.sh
@@ -1,12 +1,31 @@
-curl --include --request POST http://localhost:4741/cars \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}" \
-  --data '{
-    "car": {
-      "user_id": "1",
-      "make": "Mazda",
-      "model": "Mazda6",
-      "year": "2016",
-      "color": "white"
-    }
-  }'
+# curl --include --request POST http://localhost:4741/cars \
+#   --header "Content-Type: application/json" \
+#   --header "Authorization: Token token=${TOKEN}" \
+#   --data '{
+#     "car": {
+#       "user_id": "1",
+#       "make": "Mazda",
+#       "model": "Mazda6",
+#       "year": "2016",
+#       "color": "white"
+#     }
+#   }'
+#
+
+  API="${API_ORIGIN:-http://localhost:4741}"
+  URL_PATH="/cars"
+  curl "${API}${URL_PATH}" \
+    --include \
+    --request POST \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Token token=$TOKEN" \
+    --data '{
+      "car": {
+        "make": "'"${MAKE}"'",
+        "model": "'"${MODEL}"'",
+        "year": "'"${YEAR}"'",
+        "color": "'"${COLOR}"'"
+      }
+    }'
+
+  echo
